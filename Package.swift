35d97c44b65dd193452845b0aa681c28c41ccc3e
rollmind <eyerama@gmail.com>
2019-06-14 00:49:56 +0900

    Text extension for StringProtocol

--- Package.swift
+++ Package.swift
@@ -24,5 +24,6 @@ let package = Package(
         .testTarget(
             name: "SwiftUIExtensionsTests",
             dependencies: ["SwiftUIExtensions"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
--- Sources/SwiftUIExtensions/SwiftUIExtensions.swift
+++ Sources/SwiftUIExtensions/SwiftUIExtensions.swift
@@ -1,3 +1,19 @@
-struct SwiftUIExtensions {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct ViewConverter<Source: ViewConverterWrappable> {
+    let source: Source
+}
+
+public protocol ViewConverterWrappable {
+    var view: ViewConverter<Self> { get }
+}
+
+public extension ViewConverterWrappable {
+    var view: ViewConverter<Self> { return ViewConverter(source: self) }
+}
+
+extension String: ViewConverterWrappable {}
+@available(OSX 10.15, *)
+public extension ViewConverter where Source: StringProtocol {
+    var text: Text { return Text(self.source) }
 }
