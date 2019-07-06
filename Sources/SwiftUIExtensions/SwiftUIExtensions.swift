d213a4bb861d708c196f7c8d30483be6c42aa816
rollmind <eyerama@gmail.com>
2019-07-07 00:32:46 +0900

    Add Color conversion from UIColor

--- Sources/SwiftUIExtensions/SwiftUIExtensions.swift
+++ Sources/SwiftUIExtensions/SwiftUIExtensions.swift
@@ -1,4 +1,11 @@
+import Foundation
 import SwiftUI
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#else
+#endif
 
 public struct ViewConverter<Source: ViewConverterWrappable> {
     let source: Source
@@ -16,4 +23,26 @@ extension String: ViewConverterWrappable {}
 @available(OSX 10.15, iOS 13.0, *)
 public extension ViewConverter where Source: StringProtocol {
     var text: Text { return Text(self.source) }
+}
+
+@available(OSX 10.15, iOS 13.0, *)
+extension Color: ViewConverterWrappable {}
+@available(OSX 10.15, iOS 13.0, *)
+public extension ViewConverter where Source == Color {
+    private func convert(_ handler: (UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?) -> Void) -> (Double, Double, Double, Double) {
+        var hsba: (CGFloat, CGFloat, CGFloat, CGFloat) = (0.0, 0.0, 0.0, 0.0)
+        handler(&hsba.0, &hsba.1, &hsba.2, &hsba.3)
+        return (Double(hsba.0), saturation: Double(hsba.1), brightness: Double(hsba.2), alpha: Double(hsba.3))
+    }
+    
+    #if os(iOS)
+    typealias SourceColor = UIColor
+    #elseif os(macOS)
+    typealias SourceColor = NSColor
+    #else
+    #endif
+    func color(from color: SourceColor) -> Color {
+        let hsba = self.convert(color.getHue)
+        return Color(hue: hsba.0, saturation: hsba.1, brightness: hsba.2, opacity: hsba.3)
+    }
 }
