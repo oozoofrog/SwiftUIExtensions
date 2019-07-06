3b3d31530b7656a26e3ebb5e6f2e70138704e188
rollmind <eyerama@gmail.com>
2019-07-07 00:52:23 +0900

    Add convert to Color from UIColor

--- Sources/SwiftUIExtensions/SwiftUIExtensions.swift
+++ Sources/SwiftUIExtensions/SwiftUIExtensions.swift
@@ -15,34 +15,43 @@ public protocol ViewConverterWrappable {
     var swiftUI: ViewConverter<Self> { get }
 }
 
-public extension ViewConverterWrappable {
+public extension ViewConverterWrappable where Self: NSObject {
     var swiftUI: ViewConverter<Self> { return ViewConverter(source: self) }
 }
 
-extension String: ViewConverterWrappable {}
+extension String: ViewConverterWrappable {
+    public var swiftUI: ViewConverter<String> { return ViewConverter(source: self) }
+}
 @available(OSX 10.15, iOS 13.0, *)
 public extension ViewConverter where Source: StringProtocol {
     var text: Text { return Text(self.source) }
 }
 
 @available(OSX 10.15, iOS 13.0, *)
-extension Color: ViewConverterWrappable {}
+extension Color: ViewConverterWrappable {
+    public var swiftUI: ViewConverter<Color> { return ViewConverter(source: self) }
+}
+#if os(iOS)
+public typealias SourceColor = UIColor
+#elseif os(macOS)
+public typealias SourceColor = NSColor
+#else
+#endif
 @available(OSX 10.15, iOS 13.0, *)
-public extension ViewConverter where Source == Color {
-    private func convert(_ handler: (UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?) -> Void) -> (Double, Double, Double, Double) {
+public extension Color {
+    static private func convert(_ handler: (UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?, UnsafeMutablePointer<CGFloat>?) -> Void) -> (Double, Double, Double, Double) {
         var hsba: (CGFloat, CGFloat, CGFloat, CGFloat) = (0.0, 0.0, 0.0, 0.0)
         handler(&hsba.0, &hsba.1, &hsba.2, &hsba.3)
         return (Double(hsba.0), saturation: Double(hsba.1), brightness: Double(hsba.2), alpha: Double(hsba.3))
     }
-    
-    #if os(iOS)
-    typealias SourceColor = UIColor
-    #elseif os(macOS)
-    typealias SourceColor = NSColor
-    #else
-    #endif
-    func color(from color: SourceColor) -> Color {
+    static func color(from color: SourceColor) -> Color {
         let hsba = self.convert(color.getHue)
         return Color(hue: hsba.0, saturation: hsba.1, brightness: hsba.2, opacity: hsba.3)
+    }
+}
+@available(OSX 10.15, iOS 13.0, *)
+public extension ViewConverter where Source: SourceColor {
+    var color: Color {
+        return Color.color(from: self.source)
     }
 }
